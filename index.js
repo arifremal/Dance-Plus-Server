67901287fdf14a538af5285a93e8d69e45206459
arifremal <arifremal013@gmail.com>
2023-06-11 17:40:53 +0600

    successfully item added in enroll database

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const express = require('express')
-const app =express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // middleware
 require("dotenv").config();
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@aremal.jgqn0oc.mongodb.net/?retryWrites=true&w=majority`
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@aremal.jgqn0oc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,32 +25,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const clssesCollections = client.db("dnacePlusDB").collection('popularClasses')
-const instructorsCollections = client.db("dnacePlusDB").collection('popularInstructors')
-const enrolledCollections = client.db("dnacePlusDB").collection('enrollled')
+    const clssesCollections = client
+      .db("dnacePlusDB")
+      .collection("popularClasses");
+    const instructorsCollections = client
+      .db("dnacePlusDB")
+      .collection("popularInstructors");
+    const enrolledCollections = client
+      .db("dnacePlusDB")
+      .collection("enroll");
 
+    app.get("/class", async (req, res) => {
+      const result = await clssesCollections.find().toArray();
+      res.send(result);
+    });
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsCollections.find().toArray();
+      res.send(result);
+    });
 
-app.get('/class',async(req,res)=>{
-  const result = await clssesCollections.find().toArray()
-  res.send(result)
-})
-app.get('/instructors',async(req,res)=>{
-  const result = await instructorsCollections.find().toArray()
-  res.send(result)
-})
-
-// enrolled students 
-app.post('enrollled',async (req,res)=>{
-  const item = req.body
-  const result = await enrolledCollections.insertOne(item)
-  res.send(result)
-
-})
-
+    // enrolled students
+    app.post("/enroll", async (req, res) => {
+      const item = req.body;
+      const result = await enrolledCollections.insertOne(item);
+      console.log(result);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -63,10 +64,9 @@ app.post('enrollled',async (req,res)=>{
 }
 run().catch(console.dir);
 
-
-app.get('/',(req,res)=>{
-    res.send('running bro')
-})
-app.listen(port,()=>{
-    console.log('running on port 5000');
-})
+app.get("/", (req, res) => {
+  res.send("running bro");
+});
+app.listen(port, () => {
+  console.log("running on port 5000");
+});
