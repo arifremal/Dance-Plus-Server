0c8e8a223a2ef4fbb29649081f5419f09db497c2
arifremal <arifremal013@gmail.com>
2023-06-12 20:46:30 +0600

    api created for as different user rool

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const  jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
@@ -9,6 +9,26 @@ require("dotenv").config();
 app.use(cors());
 
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Do not have access in this router" });
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (er) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Do not have access in this router" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@aremal.jgqn0oc.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -35,13 +55,11 @@ async function run() {
     const enrolledCollections = client.db("dnacePlusDB").collection("enroll");
     const usersCollections = client.db("dnacePlusDB").collection("users");
 
-
-
-    app.post('/jwt',(req,res)=>{
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
-      res.send(token)
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res.send(token);
+    });
 
     // get all users
 
@@ -63,6 +81,30 @@ async function run() {
       const result = await usersCollections.insertOne(user);
       res.send(result);
     });
+    // app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+    //   const email = req.params.email;
+
+    //   if(req.decoded.email !== email){
+    //     res.send({admin: false})
+    //   }
+    //   const query = { email: email };
+    //   const user = await usersCollections.findOne(query);
+    //   const result = { admin: user?.role === "admin" };
+    //   console.log(result);
+    //   res.send(result)
+    // });
+
+
+    app.get('/users/admin/:email', async(req,res)=>{
+      const email = req.params.email
+    
+      
+      const query = {email: email }
+      const user = await usersCollections.findOne(query)
+      const result = {admin: user?.role === "admin"}
+      res.send(result)
+
+    } )
 
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
@@ -75,6 +117,18 @@ async function run() {
       const result = await usersCollections.updateOne(filtering, updateDoc);
       res.send(result);
     });
+// admin check
+app.get('/users/instructor/:email', async(req,res)=>{
+  const email = req.params.email
+
+  
+  const query = {email: email }
+  const user = await usersCollections.findOne(query)
+  const result = {instructor: user?.role === "instructor"}
+  res.send(result)
+
+} )
+
     // make instructor
     app.patch("/users/instructor/:id", async (req, res) => {
       const id = req.params.id;
@@ -117,11 +171,19 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/enroll", async (req, res) => {
+    app.get("/enroll", verifyJWT, async (req, res) => {
       const email = req.query.email;
+
       if (!email) {
         res.send([]);
       }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden Access" });
+      }
+
       const query = { email: email };
       const result = await enrolledCollections.find(query).toArray();
       res.send(result);
